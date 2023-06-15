12eb5bef102d919419141793bf5fecdc104d7f99
miacw_ <miawallace1999@hotmail.co.uk>
2023-06-15 20:02:57 +0100

    Update Nav.js

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -51,7 +51,7 @@ const Nav = (props) => {
   return (
     <div className="nav">
       <Link to="/">
-        <div className="logo" onClick={updateMenu}>
+        <div className="logo" onClick={setIsMenuClicked(false)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 207.3 50.1"
