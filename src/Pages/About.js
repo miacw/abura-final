a4f802007577b7ba8dee39e1d9de0609a7d49636
miacw_ <miawallace1999@hotmail.co.uk>
2023-06-16 11:43:47 +0100

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -14,18 +14,18 @@ const About = ({ setTitle }) => {
         <p className="about-text">
           Abura is an African cocktail and art bar where the vibrant tapestry of
           the black African diaspora comes to life. At its core, Abura is a
-          testament to their culturalimpact and ever evolving story.
+          testament to their cultural impact and ever evolving story.
         </p>
         <p className="about-text">
           In Twi, 'Abura' means 'drinking well', and our vision is to be a place
-          where thecommunity comes together, to quench their souls for
-          connection. We fuse premiumAfrican beverages with recipe blends
-          cherished by the diaspora, and marvel at thecaptivating artwork
+          where the community comes together, to quench their souls for
+          connection. We fuse premium African beverages with recipe blends
+          cherished by the diaspora, and marvel at the captivating artwork
           produced by diaspora talent.
         </p>
         <p className="about-text">
           Abura is more than a bar; it is a sanctuary for the diaspora community
-          and aninvitation to all to immerse themselves in the beauty of their
+          and an invitation to all to immerse themselves in the beauty of their
           experience.
         </p>
       </div>
